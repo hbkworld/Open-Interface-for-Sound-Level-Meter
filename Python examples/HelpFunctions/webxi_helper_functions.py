7f2e035cff4393a588296d63e1b6e092a505f268
Sofus Vingaard <sofus.krebsvingaard@hbkworld.com>
2026-08-28 11:33:07 +0200

    made a helper function to set the host ip

--- Python examples/HelpFunctions/webxi_helper_functions.py
+++ Python examples/HelpFunctions/webxi_helper_functions.py
@@ -1,4 +1,6 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 # Functions to turn on/off BB freq weights and sequences. These are used in the examples to make sure the wanted sequences are enabled on the SLM.
 def turn_off_bb_freq_weight(host):
@@ -29,3 +31,10 @@ def turn_on_CPB_freq_weight(host, weights):
 def turn_on_CPB_l_eq(host, weights):
     for weight in weights:
         requests.put(host + f"/webxi/Applications/SLM/Setup/CPB{weight}", json=True)
+
+def set_host_ip(caller_file):
+    load_dotenv(dotenv_path=os.path.join(os.path.dirname(caller_file), ".env"))
+    ip = os.getenv("IP")
+    if not ip:
+        raise RuntimeError('Missing IP. Set IP in a .env file (IP=...) or environment variable.')
+    return f"http://{ip}", ip
