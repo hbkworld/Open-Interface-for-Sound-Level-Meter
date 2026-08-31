e0f105380d247ff74caec49718e7fd94be8c8056
Sofus Vingaard <sofus.krebsvingaard@hbkworld.com>
2026-08-31 13:26:38 +0200

    updated example 1-5 to use the set_ip function.

--- Python examples/05 - ReadLafPerSecond.py
+++ Python examples/05 - ReadLafPerSecond.py
@@ -6,17 +6,9 @@
 import requests # Handle reply from server
 import pprint as pp # Pretty Print to nicely print out data from the response
 import time # For the 'sleep' funtion
-from dotenv import load_dotenv
-import os
+from HelpFunctions import webxi_helper_functions as webxi_helper
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
-
-ip = os.getenv("IP")
-if not ip:
-
-    raise RuntimeError('Missing IP. Set IP in a .env file (IP=...) or environment variable.')
-
-host = f"http://{ip}"
+host, ip = webxi_helper.set_host_ip(__file__)
 
 
 """
